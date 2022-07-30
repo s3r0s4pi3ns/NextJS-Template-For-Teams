d8927436e14975b00543696a6d0f8ac5960d9bee
s3r0s4pi3ns <s3r0s4pi3ns@protonmail.com>
2022-07-30 17:00:20 +0100

    initial nextjs configuration

---
+++ next.config.js
@@ -0,0 +1,73 @@
+/** @type {import('next').NextConfig} */
+
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src *.google.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // i18n: {
+  //   locales: ['es', 'en'],
+  //   defaultLocale: 'en',
+  // },
+  optimizeFonts: true,
+  poweredByHeader: false,
+  //https://nextjs.org/docs/advanced-features/security-headers
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\n/g, ""),
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Server",
+            value: "Apache", // phony server value
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
